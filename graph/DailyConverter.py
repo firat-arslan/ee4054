72c6e9cd277e7c46b3b84489ea4b54ae54960e78
firat-arslan <92940062+firat-arslan@users.noreply.github.com>
2021-11-16 12:43:10 +0300

    Update DailyConverter.py

--- graph/DailyConverter.py
+++ graph/DailyConverter.py
@@ -9,12 +9,3 @@ def convert(path_input_day, path_output, household, start_date, end_date):
     df_day_mask = df_day[df_day.day.between(start_date, end_date)]
     df_day_mask.to_csv(path_output, index=False, header=True, float_format='%.4f')
 
-
-path_input_day = "input-tables/block_0.csv"
-path_output = "output-tables/block_0_day_converted.csv"
-
-start_date_daily = '2014-01-01'
-end_date_daily = '2014-01-08'
-
-convert(path_input_day, path_output, 'MAC000002', start_date_daily, end_date_daily)
-
